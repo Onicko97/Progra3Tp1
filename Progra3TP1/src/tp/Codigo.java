856cbf7d08af862f08e61c25687d7c5fff19f95c
Nick97 <nicobilchez@gmail.com>
2026-03-23 15:45:09 -0300

    test inicial con un teclado hecho de Jbuttons

---
+++ Progra3TP1/src/tp/Codigo.java
@@ -0,0 +1,27 @@
+package tp;
+
+import java.util.Random;
+
+import javax.swing.JTextField;
+
+public class Codigo {
+	
+	String listaPalabras[] = {"hola", "pera", "miel", "sapo" , "pala", "hilo", "vaso"};
+	
+	public String damePalabra() {
+		Random random = new Random();
+		int indice = random.nextInt(listaPalabras.length);
+		return listaPalabras[indice];
+	}
+	
+	public void botonPresionado(JTextField txt, String boton) {
+		if(boton == "delete") {
+			String aux = txt.getText();
+			aux = aux.substring(0, aux.length()-1);
+			txt.setText(aux);
+		}
+		else {
+			txt.setText(txt.getText() + boton);
+		}
+	}
+}
