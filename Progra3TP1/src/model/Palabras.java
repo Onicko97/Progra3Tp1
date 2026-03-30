9400d7563e483fdea1f61a0be7f9330d7e321948
Nick97 <nicobilchez@gmail.com>
2026-03-30 17:29:40 -0300

    sin cambios

--- Progra3TP1/src/model/Palabras.java
+++ Progra3TP1/src/model/Palabras.java
@@ -7,8 +7,14 @@ import java.util.Set;
 public class Palabras {
 
 	private final static Set<String> palabrasWordle = new HashSet<>(Set.of("perro", "casas", "viejo", "sonar"));
+	//private final static Set<String> palabrasWordle = new HashSet<>();
 	
 	public static String obtenerPalabraRandom() {
+
+
+//		for (String string : palabrasWordle) {
+//			palabrasWordle.add(string);
+//		}
 		Random random = new Random();
 		int numRandom = random.nextInt(palabrasWordle.size());
 		String[] palabras = palabrasWordle.toArray(new String[palabrasWordle.size()]);
